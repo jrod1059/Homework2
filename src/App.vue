74b7b0200bf93d88c7644549967377ea26ee7cb8
Jonathan Rodriguez <rodriguez.jonathan77@yahoo.com>
2025-02-09 19:30:21 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import{ref} from 'vue'
+
+const name = ref('')
+</script>
+
+<template>
+
+<main class="app">
+  <section class="greeting">
+  <h2 class="title">
+  Welcome to your Ingridients list<input type="text" placeholder="Enter Name" v-model="name">
+  </h2>
+  </section>
+
+  <section class="Ingridients">
+  <div class = "Ingridients list">
+
+  <input type="text" placeholder="Your Ingredients!" v-model="Your_list" />
+  </div>
+
+  </section>
+
+  <section class ="Add to list">
+  </section>
+
+  <section class ="Delete from list">
+  </section>
+  
+
+
+</main>
+
+
+
+</template>
